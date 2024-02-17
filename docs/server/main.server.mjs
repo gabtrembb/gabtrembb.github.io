bbe8e888a9c73b43a7f27f588810b2c251226703
gabtrembb <angel_gabi98@hotmail.com>
2024-02-16 19:18:44 -0500

    changed amount and question size problem from iphone

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GAK5NNNU.mjs";import"./chunk-43ODLSH2.mjs";import"./chunk-3JANUDEC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-GOMFKESB.mjs";import"./chunk-43ODLSH2.mjs";import"./chunk-3JANUDEC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
